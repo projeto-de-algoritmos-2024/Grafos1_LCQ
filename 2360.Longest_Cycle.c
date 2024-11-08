5e1265370675569dfbc4aa11850c5253386019c4
Leandro <leandroalmeidaoliveira650@gmail.com>
2024-11-08 16:32:00 -0300

    add dfs
    Co-authored-by: Muniz2811 <pedrohenben2014@gmail.com>

--- 2360.Longest_Cycle.c
+++ 2360.Longest_Cycle.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct no {
     int valor;
@@ -9,28 +8,66 @@ typedef struct no {
 
 typedef struct pilha {
     no *topo;
-    int contador;
+    int cont;
 } pilha;
 
-void inicializar_pilha(pilha *p) {
+void criandoPilha(pilha *p) {
     p->topo = NULL;
-    p->contador = 0;
+    p->cont = 0;
 }
 
-void inserir_na_pilha(pilha *p, int x) {
-    no *novo = malloc(sizeof(no));
+int pilhaVazia(pilha *p) {
+    return p->topo == NULL;
+}
+
+void empilhar(pilha *p, int x) {
+    no *novo = (no *)malloc(sizeof(no));
     novo->valor = x;
     novo->prox = p->topo;
     p->topo = novo;
-    p->contador++;
+    p->cont++;
 }
 
-void remover_da_pilha(pilha *p) {
-    if (p->topo != NULL) {
-        no *lixo = p->topo;
-        p->topo = lixo->prox;
-        lixo->prox = NULL;
-        free(lixo);
-        p->contador--;
+int desempilhar(pilha *p) {
+    if (pilha_vazia(p)) {
+        return -1;
     }
+    no *aux = p->topo;
+    int valor = aux->valor;
+    p->topo = aux->prox;
+    free(aux);
+    p->cont--;
+    return valor;
+}
+
+void dfs(int *aresta, int numVertices, int inicio) {
+    pilha p;
+    criandoPilha(&p);
+
+    int *visitado = (int *)malloc(numVertices * sizeof(int));
+    for (int i = 0; i < numVertices; i++) visitado[i] = 0;
+
+    empilhar(&p, inicio);
+    visitado[inicio] = 1;
+
+    while (!pilha_vazia(&p)) {
+        int atual = desempilhar(&p);
+        printf("%d ", atual);
+
+        int prox = aresta[atual];
+        if (prox != -1 && !visitado[prox]) {
+            empilhar(&p, prox);
+            visitado[prox] = 1;
+        }
+    }
+}
+
+int main() {
+    int numVertices = 6;
+    int aresta[] = {-1, 2, 3, -1, 5, -1}; 
+
+    int inicio = 1; 
+    dfs(aresta, numVertices, inicio);
+
+    return 0;
 }
